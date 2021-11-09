0addcc8215fc04dee4e6521cd406d9e4facffe43
Adwait Raste <rasteadwait@gmail.com>
2021-11-09 13:44:47 +0530

    Trying to integrate charting

--- src/Components/Chart.js
+++ src/Components/Chart.js
@@ -1,37 +1,56 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Bar, Line, Pie } from 'react-chartjs-2';
 
 const Chart = () => {
 
     const [jsonData, setJsonData] = useState([]);
-    const [labels, setLabels] = useState([]);
+    const [myLabels, setMyLabels] = useState([]);
     const [myData, setMyData] = useState([]);
-
+    const [loading, setLoading] = useState(true);
+    const isInitialMount = useRef(true);
 
     useEffect(() => {
-        fetch("https://api.thingspeak.com/channels/1543836/fields/" + "1" + ".json?api_key=37WFEHRP9JD2266M&results=3", {
+        fetch("https://api.thingspeak.com/channels/1543836/fields/" + "1" + ".json?api_key=37WFEHRP9JD2266M&results=10", {
             method: 'GET',
         })
             .then(res => res.json())
             .then(data => {
                 setJsonData(data["feeds"]);
-                jsonData.map( e => {
-                    setLabels([...labels, e["created_at"]]);
-                    setMyData([...myData, e['field1']]);
-                })
             })
-    }
-    )
+    }, [])
+
+    useEffect(() => {
+        if (isInitialMount.current) {
+            isInitialMount.current = false;
+         } else {
+            jsonData.map(e => {
+                setMyLabels([...myLabels, e["created_at"]]);
+                setMyData([...myData, e['field1']]);
+            })
+            setLoading(false)
+         }
+    }, [jsonData])
 
 
     return (
         <div>
-            <Bar
-                data={null}
-                width={100}
-                height={50}
-                options={{ maintainAspectRatio: false }}
-            />
+            {   !loading &&
+
+                <Line
+                    data={{
+                        labels: myLabels,
+                        datasets: [
+                            {
+                                label: "Temperature",
+                                data: myData,   
+                            }
+                        ]
+                    }}
+                    width={600}
+                    height={400}
+                    options={{ maintainAspectRatio: false }}
+                />
+            }
         </div>
     );
 }
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,20 +2,21 @@ import React from 'react';
 import { Container } from 'reactstrap';
 import AppNavbar from '../Components/AppNavbar';
 import FieldTable from '../Components/FieldTable';
+import Chart from '../Components/Chart';
 
 const Home = () => 
 {
     return (
         <div>
             <AppNavbar />
-            <Container>
             <br/>
+            <Chart/>
             <FieldTable name="Temperature"/>
             <FieldTable name="pH"/>
             <FieldTable name="TDS"/>
             <FieldTable name="Turbidity"/>
-        </Container>
         </div>   
+        
     )
 }
 
